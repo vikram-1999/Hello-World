1c1f5cf0136d776b659827d1a106dfdec91c5327
vikram-1999 <vikramsingh849907@gmail.com>
2021-04-19 16:18:43 -0700

    first commit

---
+++ src/Test2.java
@@ -0,0 +1,21 @@
+
+public class Test2 {
+	
+	int a=0;
+	
+	void m1() {
+		a=+10;
+		System.out.println(a);
+	}
+	void m2() {
+		a=+10;
+		System.out.println(a);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test2 t = new Test2();
+		t.m1();
+		t.m2();
+	}
+
+}
